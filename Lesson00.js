4c5ae20b9166a6a816ef7aae0fa72abbc3c227dc
magepaca <pabonmario@gmail.com>
2021-09-13 11:51:58 -0500

    Var declaration

---
+++ Lesson00.js
@@ -0,0 +1,17 @@
+console.log("Hello from JavaScript");
+// in-line comment
+/* long comment  this is multi-line comment
+there can be many lines of comment
+*/
+// Var declaration
+var number = 5;
+/* Data types undefined, null, boolean, string, symbol, number, and object */
+// boolean is true or false
+// var declaration
+var myName = "Mario"; // var can be used through the whole program
+// let declaration
+let ourName = "My first project"; // let can be used only within the scop of where you declare that
+
+// const declaration
+const pi = 3.14; // const cannot be changed
+
