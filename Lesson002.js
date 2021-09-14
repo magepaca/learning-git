2d892458df935194541c83a151af0e05d6667007
magepaca <pabonmario@gmail.com>
2021-09-13 22:20:59 -0500

    First commitment lesson 002

---
+++ Lesson002.js
@@ -0,0 +1,10 @@
+//first function
+function wordBlank(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result = "the " + " " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+    return result;
+}
+console.log(wordBlank("dog", "big", "ran", "slowly"));
+console.log(wordBlank("cat", "black", "sleeps", "daily"));
+
+//Array example
